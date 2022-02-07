3d9f2606aec62c3bed3ac1cf7e2121b78a328c11
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-02-07 11:24:58 +0100

    Ejercicio 1

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,3 +1,4 @@
+//Modificar un elemento del HTML para agregarle un texto adicional
 function Ejercicio1() {
     var explicacion = document.getElementById("explicacion")
     explicacion.innerHTML = explicacion.innerText + " Te damos la bienvenida"
