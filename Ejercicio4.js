5cddbc229d499811f9c28bb7a79c07571c1177b1
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-02-07 11:45:37 +0100

    Ejercicio 4

---
+++ Ejercicio4.js
@@ -0,0 +1,4 @@
+function Ejercicio4() {
+    var recreo = document.getElementById("cuartaHora")
+    recreo.style.backgroundColor = "blue"
+}
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function main() {
     Ejercicio1()
-    Ejercicio2()
+    //Ejercicio2()
     Ejercicio3()
+    Ejercicio4()
 
 }
 main()
